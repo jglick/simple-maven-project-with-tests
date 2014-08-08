7c9c86ea6095a1451c860ff51ab6d054811f5a94
Jesse Glick <jglick@cloudbees.com>
2014-08-08 10:36:14 -0400

    TestNG variant.

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -1,16 +1,15 @@
 package test;
 
-import static org.junit.Assert.*;
-import org.junit.internal.AssumptionViolatedException;
+import org.testng.SkipException;
 
 class Base {
 
     protected void run() {
         double r = Math.random();
         if (r < 0.1) {
-            fail("oops");
+            assert false : "oops";
         } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping");
+            throw new SkipException("skipping");
         }
     }
 
--- src/test/java/test/OtherTest.java
+++ src/test/java/test/OtherTest.java
@@ -1,6 +1,6 @@
 package test;
 
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 public class OtherTest extends Base {
 
