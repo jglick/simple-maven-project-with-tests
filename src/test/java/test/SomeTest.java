9aeacf7ab307933abbd808ba9586759d0e89b171
Headbanger68 <putzteufel@e-mail.de>
2020-11-30 04:50:14 +0100

    test1

--- src/test/java/test/SomeTest.java
+++ src/test/java/test/SomeTest.java
@@ -27,11 +27,11 @@ package test;
 import org.junit.Test;
 
 public class SomeTest extends Base {
-
+/*
     @Test public void test1() {
         run();
     }
-
+*/
 
     @Test public void test2() {
         run();
