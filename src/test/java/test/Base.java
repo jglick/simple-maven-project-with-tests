474e128d56927cc56d719351baf34abb2bb3fee2
Julien Greffe <jgreffe@cloudbees.com>
2023-10-05 12:55:22 +0200

    Parametrized skipping.odd

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,10 +31,11 @@ class Base {
 
     protected void run() {
         int failureOdd = Integer.valueOf(System.getProperty("failure.odd", "0.1"));
+        int skippingOdd = Integer.valueOf(System.getProperty("skipping.odd", "0.2"));
         double r = Math.random();
         if (r < failureOdd) {
             fail("oops");
-        } else if (r < 0.2) {
+        } else if (r < skippingOdd) {
             throw new AssumptionViolatedException("skipping");
         }
     }
