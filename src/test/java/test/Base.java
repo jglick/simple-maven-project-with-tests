dba89ef5deffcbbebbd5fe087a4a2bdba863afcc
Julien Greffe <jgreffe@cloudbees.com>
2023-10-05 13:00:05 +0200

    Proper double

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -30,8 +30,8 @@ import org.junit.internal.AssumptionViolatedException;
 class Base {
 
     protected void run() {
-        double failureOdd = Double.valueOf(System.getProperty("failure.odd", "0.1"));
-        double skippingOdd = Double.valueOf(System.getProperty("skipping.odd", "0.2"));
+        double failureOdd = Double.parseDouble(System.getProperty("failure.odd", "0.1"));
+        double skippingOdd = Double.parseDouble(System.getProperty("skipping.odd", "0.2"));
         double r = Math.random();
         if (r < failureOdd) {
             fail("oops");
